262b25fb63456ea7c3d064a0dcae448eddf2bf33
leantony <chachaantony@gmail.com>
2017-04-04 10:02:49 +0300

    register command

--- src/providers/ServiceProvider.php
+++ src/providers/ServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Leantony\Database\Providers;
 
 use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
+use Leantony\Database\Commands\GenerateRepository;
 
 class ServiceProvider extends LaravelServiceProvider
 {
@@ -16,5 +17,15 @@ class ServiceProvider extends LaravelServiceProvider
         $this->publishes([
             __DIR__ . '/../config/repository.php' => config_path('repository.php')
         ], 'config');
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->commands(GenerateRepository::class);
     }
 }
